8bd0250ec1b6b7b7ddd1b6d123bb6ecf02585387
Sunil Kumar <sunilkumar1996.r@gmail.com>
2025-08-18 15:45:02 +0530

    Update reprot controllers

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import { getSalesReport, getTopProducts, getInventoryAlerts } from '../controllers/reportController.js';
-import {verifyToken} from '../middlewares/authMiddleware.js';
+import { getSalesRange, getSalesFrom, getSalesTo, getTopProductsRange, getTopProductsFrom, getTopProductsTo, getInventoryAlerts } from '../controllers/reportController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/sales',verifyToken, getSalesReport);
-router.get('/products/top',verifyToken, getTopProducts);
-router.get('/inventory',verifyToken, getInventoryAlerts);
+// SALES
+router.get('/sales/range', getSalesRange);   // requires from & to
+router.get('/sales/from', getSalesFrom);     // only from
+router.get('/sales/to', getSalesTo);         // only to
+
+// TOP PRODUCTS
+router.get('/top-products/range', getTopProductsRange);
+router.get('/top-products/from', getTopProductsFrom);
+router.get('/top-products/to', getTopProductsTo);
+
+//Inventory Alerts
+router.get('/inventory', verifyToken, getInventoryAlerts);
 
 export default router;
