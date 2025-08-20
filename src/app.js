a540509f70e3fd57dbcdbd1d45027f7cc350f776
Sunil Kumar <sunilkumar1996.r@gmail.com>
2025-08-20 12:05:48 +0530

    cors updated1

--- src/app.js
+++ src/app.js
@@ -36,9 +36,16 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Enable CORS
+// app.use(cors({
+//   origin:'http://localhost:5173',
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin:'http://localhost:5173',
-  credentials: true,
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // if using cookies or auth headers
 }));
 
 // API Routes
