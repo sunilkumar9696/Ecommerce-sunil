404f0895952cf76174a5c83824c1c982ba20cc12
Sunil Kumar <sunilkumar1996.r@gmail.com>
2025-08-20 11:15:02 +0530

    removed token

--- src/routes/wishlistRoutes.js
+++ src/routes/wishlistRoutes.js
@@ -4,8 +4,8 @@ import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/add', verifyToken, addToWishlist);
-router.delete('/remove/:productId', verifyToken, removeFromWishlist);
-router.get('/', verifyToken, getWishlist);
+router.post('/add', addToWishlist);
+router.delete('/remove/:productId', removeFromWishlist);
+router.get('/', getWishlist);
 
 export default router;
