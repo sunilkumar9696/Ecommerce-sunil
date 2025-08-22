6c5781c36a01c754031f39ad56075d214d47e994
Sunil Kumar <sunilkumar1996.r@gmail.com>
2025-08-22 10:20:32 +0530

    checking update-profile

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 router.post('/request-otp', requestOTP);
 router.post('/verify-otp', verifyOTP);
-router.put('/update-Profile', updateProfile);
+router.put('/update-Profile' ,verifyToken, updateProfile);
 
 export default router;
